bb945a07d5afc4424acf6bcc241c6340d79c1daf
Pratik Patil <pratikpatil.dtis@gmail.com>
2022-05-07 15:01:09 +0530

    Time: 587 ms (22.45%), Space: 32.1 MB (33.50%) - LeetHub

--- 456-132-pattern/456-132-pattern.py
+++ 456-132-pattern/456-132-pattern.py
@@ -1,10 +1,13 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
+        # stack will contain = [top,minLeft]
         stack = []
         
         currMin = nums[0]
         
-        for curr in nums:
+        # try to compare minLeftToStack < curr < stackTop 
+        for curr in nums[1:]:
+            #make sure your stack is strictly increasing from bottom so pop other and keep highest valur at top.
             while stack and stack[-1][0] <= curr:
                 stack.pop()
                 
