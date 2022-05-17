e343cf914c8adc9145c401f0566304412b2782b5
Pratik Patil <pratikpatil.dtis@gmail.com>
2022-05-17 09:55:33 +0530

    Time: 1158 ms (8.20%), Space: 24.1 MB (22.40%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -7,12 +7,25 @@
 
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
-        def inorder(o: TreeNode, c: TreeNode):
-            if o:
-                inorder(o.left, c.left)
-                if o is target:
-                    self.ans = c
-                inorder(o.right, c.right)
+        stack_o, stack_c = [], []
+        node_o, node_c = original, cloned
+        
+        while stack_o or node_c:
+            while node_o:
+                stack_o.append(node_o)
+                stack_c.append(node_c)
+                
+                node_o = node_o.left
+                node_c = node_c.left
+                
+            node_o = stack_o.pop()
+            node_c = stack_c.pop()
+            
+            if node_o is target:
+                return node_c
+            
+            node_o = node_o.right
+            node_c = node_c.right 
                 
         inorder(original, cloned)
         return self.ans 
