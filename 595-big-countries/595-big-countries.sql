2c4075cc1897b9c613de592dfb563aa14e6c8c81
Pratik Patil <pratikpatil.dtis@gmail.com>
2022-06-07 22:15:49 +0530

    Time: 262 ms (79.94%), Space: 0B (100.00%) - LeetHub

--- 595-big-countries/595-big-countries.sql
+++ 595-big-countries/595-big-countries.sql
@@ -5,3 +5,15 @@ FROM
     World w
 WHERE
     w.area >= 3000000 OR w.population >= 25000000
+    
+    
+#Union
+#SELECT name, population, area
+#FROM World
+#WHERE area >= 3000000 
+
+#UNION
+
+#SELECT name, population, area
+#FROM World
+#WHERE population >= 25000000
