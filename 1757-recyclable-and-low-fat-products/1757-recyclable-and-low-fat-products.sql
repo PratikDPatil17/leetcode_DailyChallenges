640a27c0e79ccf2ef72bc8328c84b77e8b3d7b02
Pratik Patil <pratikpatil.dtis@gmail.com>
2022-06-07 22:11:48 +0530

    Time: 731 ms (14.69%), Space: 0B (100.00%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
+
+# Facebook question to get percentage of healthy food 
+#SELECT ROUND(AVG(CASE WHEN low_fats = 'Y' AND recyclable = 'Y' THEN 1 ELSE 0 END),2)*100 AS PERCENTAGE
+
 SELECT
     product_id
 FROM
