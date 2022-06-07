905a644633862c02ea56b4c2d63eef2b5b0dae20
Pratik Patil <pratikpatil.dtis@gmail.com>
2022-06-07 22:22:59 +0530

    Time: 515 ms (48.24%), Space: 0B (100.00%) - LeetHub

--- 584-find-customer-referee/584-find-customer-referee.sql
+++ 584-find-customer-referee/584-find-customer-referee.sql
@@ -3,8 +3,8 @@ SELECT
     name
 FROM
     Customer
-#WHERE
-#    referee_id != 2 OR referee_id is NULL
-    
 WHERE
-    COALESCE(referee_id, 0) <> 2
+    referee_id != 2 OR referee_id is NULL
+    
+#WHERE
+#    COALESCE(referee_id, 0) <> 2
