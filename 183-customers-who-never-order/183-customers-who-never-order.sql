776ca3826a267b292f76bff430921b47a220a17a
Pratik Patil <pratikpatil.dtis@gmail.com>
2022-06-07 22:47:09 +0530

    Time: 526 ms (43.62%), Space: 0B (100.00%) - LeetHub

---
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+SELECT
+    customers.name AS Customers
+FROM
+    customers
+WHERE
+    customers.id not in 
+    (
+        SELECT customerId FROM Orders
+    )
